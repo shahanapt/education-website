0d5fa54a5ecd531cfd6b9a5d65d4a58c76000fd9
shahana pt <fshahanapt@gmail.com>
2023-05-11 13:22:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Header from "./common/header/Header";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import About from "./about/About";
+import CourseHome from "./allcourses/CourseHome";
+import Team from "./team/Team";
+import Pricing from "./pricing/Pricing";
+import Blog from "./blog/Blog";
+import Contact from "./contact/Contact";
+import Footer from "./common/footer/Footer";
+import Home from "./home/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/courses" component={CourseHome} />
+          <Route exact path="/team" component={Team} />
+          <Route exact path="/pricing" component={Pricing} />
+          <Route exact path="/journal" component={Blog} />
+          <Route exact path="/contact" component={Contact} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
